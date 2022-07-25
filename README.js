99151b2f0577a58cbfbc66f3d93f8bff60ae665f
JOHAN-7777 <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-25 20:01:09 +0100

    Add files via upload

---
+++ README.js
@@ -0,0 +1,30 @@
+## <!-- Typing SVG -->
+<p align="center">
+    <a href="https://github.com/MRCRAZY19/HALF-BOT_V1">
+        <img align="center"
+        src="https://readme-typing-svg.herokuapp.com/?size=30&width=500&lines=HI!!+I+am+HALF%20+%20BOT+..."
+            alt="Typing SVG"
+        />
+    </a>
+</p>                                
+ 
+
+
+
+
+
+- 😇 I'm not programmer
+ 
+- 👀 I’m interested on whatsapp bots
+
+- 🌱 I’m currently learning JAVASCRIPT
+- <h3 align="left">Connect with me:</h3><p>   <a href="https://instagram.com/__.sanuwa.__" target="blank"><img align="center" src="https://telegra.ph/file/a171ac66c22c5617fe27b.png" alt="https://www.instagram.com/mr_johan_shezir" height="40" width="40" /></a>
+</p>
+
+
+
+ <p align="center"> <a href="https://github.com/MRCRAZY19/HALF-BOT_V1"><img src="https://github-profile-trophy.vercel.app/?username=sanuwaofficial&no-bg=true&no-frame=false&theme=algolia"></a></p>
+
+<p align="center"> <a href="https://github.com/MRCRAZY19/HALF-BOT_V1"><img  src="http://github-readme-streak-stats.herokuapp.com?user=sanuwaofficial&theme=github-dark-blue&hide_border=false&background=DDD9DA00&stroke=00AEFF&fire=00AEFF&ring=00AEFF&currStreakNum=00AEFF&currStreakLabel=00AEFF&sideLabels=00AEFF&dates=00AEFF&sideNums=00AEFF"></a></p>
+<p align="center"> <a href="https://github.com/MRCRAZY19/HALF-BOT_V1"><img src="https://github-readme-stats.vercel.app/api?username=SANUWAOFFICIAL&theme=algolia&bg_color=DDD9DA00&text_color=00AEFF&show_icons=TRUE&icon_color=00AEFF" > </a> </p>
+<p align="center"> <a href="https://github.com/MRCRAZY19/HALF-BOT_V1"><img src="https://github-readme-stats.vercel.app/api/top-langs/?username=SANUWAOFFICIAL&hide=css,html&theme=algolia&bg_color=DDD9DA00&text_color=00AEFF" > </a> </p>
